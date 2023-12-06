ae00ef9158b68823259e191d4f8e5f65162b6148
neymarsabin <reddevil.sabin@gmail.com>
2023-12-06 18:41:42 +0545

    deprecate sqlite with postgresql
    - remove sqlite and use postgresql
    - minor changes to scraper
    - insert data from the scraper to database

--- jobs/process.documentation.ts
+++ jobs/process.documentation.ts
@@ -1,15 +1,18 @@
 import { eventTrigger } from '@trigger.dev/sdk';
-import { client } from '@gpt-assistant/trigger';
+import { v4 as uuidv4 } from 'uuid';
 import { object, string } from 'zod';
 import { JSDOM } from 'jsdom';
-import { openai } from '@gpt-assistant/helper/open.ai';
+import axios from 'axios';
+import { client } from '../trigger';
+import { openai } from '../app/helper/open.ai';
+import { prisma } from '../app/helper/prisma';
 
 client.defineJob({
     id: 'process-portfolio',
     name: "Process Portfolio",
-    version: "0.0.1"
+    version: "0.0.1",
     trigger: eventTrigger({
-        name: 'process.portfolio.event',
+        name: 'process.portfolio{}.event',
         schema: object({
             url: string(),
         })
@@ -18,5 +21,32 @@ client.defineJob({
         openai
     },
     run: async (payload, io, ctx) => {
+    //  get the data modify the data accordingly
+    // insert that data into the database
+    // this must be a io.runTask function
+        return io.runTask('grab-texts-from-portfolio', async () => {
+            try {
+                await axios.get('http://localhost:4567/posts')
+                    .then(async (response) => {
+                        response.data.forEach(async (res) => {
+                            const textUuid = uuidv4();
+                            await prisma.docs.upsert({
+                                where: {
+                                    url: res.url
+                                }, update: {
+                                    content: res.texts[0], identifier: textUuid
+                                }, create: {
+                                    url: res.url, content: res.texts[0], identifier: textUuid
+                                }
+                            })
+                        })
+                    })
+                    .catch((error) => {
+                        console.log("Error in response:: ", error);
+                    })
+            } catch (error) {
+                console.log("Error while program or the jobs: ", error);
+            }
+        })
     }
 })
--- scraper/sitemap/parser.rb
+++ scraper/sitemap/parser.rb
@@ -10,13 +10,16 @@ module Sitemap
       all_urls = sitemap.urls.to_a.map { |url| url.children[0].children.text }
       post_class_name = "post"
 
-      all_urls.map do |url|
+      url_with_texts = all_urls.map do |url|
         html = URI.open(url)
         doc = Nokogiri::HTML(html)
         elements = doc.css(".#{post_class_name}")
         texts = elements.map { |element| element.text }
-        { url: url, texts: texts }
+        unless texts.empty?
+          { url: url, texts: texts, lengths: texts.length }
+        end
       end
+      url_with_texts.compact
     end
   end
 end
